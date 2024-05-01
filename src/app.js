0974fe54d3812137a41aed4a4acf237e8fc3da4c
Bruno Bandelli <brunobandelli@gmail.com>
2024-05-01 02:08:32 -0300

    Refatoramento do codigo no controller e adição de novas funcionalidades

--- src/app.js
+++ src/app.js
@@ -33,22 +33,22 @@ routes(app);
 //     res.status(201).send("Livro cadastrado com sucesso")
 // })
 
-app.get("/livros/:id", (req, res)=>{
-    const index = buscaLivro(req.params.id)
-    res.status(200).json(livros[index])
-})
+// app.get("/livros/:id", (req, res)=>{
+//     const index = buscaLivro(req.params.id)
+//     res.status(200).json(livros[index])
+// })
 
-app.put("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo
-    res.status(200).json(livros);
-})
+// app.put("/livros/:id", (req, res) => {
+//     const index = buscaLivro(req.params.id);
+//     livros[index].titulo = req.body.titulo
+//     res.status(200).json(livros);
+// })
 
-app.delete("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    livros.splice(index, 1);
-    res.status(200).send("Livro removido com sucesso");
-});
+// app.delete("/livros/:id", (req, res) => {
+//     const index = buscaLivro(req.params.id);
+//     livros.splice(index, 1);
+//     res.status(200).send("Livro removido com sucesso");
+// });
 
 //Todas as vezes que chega um objeto via body via requisição eles chegam como string
 //201 registro criado
